45be4c07d2936089fae3e6aab873dc1db453f893
Your Name <you@example.com>
2020-04-27 13:33:18 +0800

    2020-4-27

--- src/views/cart/childComps/CartList.vue
+++ src/views/cart/childComps/CartList.vue
@@ -1,17 +1,35 @@
 <template>
-
+    <div class="cart-list">
+        <scroll class="content">
+            <li v-for = "item in cartList">{{item}}</li>
+        </scroll>
+        
+    </div>
 </template>
 
 
 
 <script>
+    import Scroll from '../../../components/scroll/Scroll.vue'
+
 export default {
-    name: "CartList"
+    name: "CartList",
+    components: {
+        Scroll
+    },
+    computed: {
+        cartList () {
+            return this. $store.getters.cartList
+        }
+    }
 }
 
 </script>
 
 
 <style>
+    .content {
+        height: calc(100% - 44px);
+    }
 
 </style>
